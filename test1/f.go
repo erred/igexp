c4f61c9a999b592fe45ef8d853a2791a44cb0ffd
Sean Liao <seankhliao@gmail.com>
2018-10-31 20:54:20 +0800

    logging test

--- test1/f.go
+++ test1/f.go
@@ -1,11 +1,44 @@
 package test1
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"strings"
+	"sync"
+
+	"cloud.google.com/go/logging"
 )
 
+var o = sync.Once
+var logger *logging.Logger
+var slog *log.Logger
+var slog2 *log.Logger
+
 func F(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(strings.Join(os.Environ(), "\n")))
+	fmt.Println("Output to STDOUT")
+	log.Println("Output to STDERR")
+
+	defer func() {
+		r := recover()
+		fmt.Println("recovered")
+		fmt.Println(r)
+	}()
+
+	o.Do(func() {
+		client, err := logging.NewClient(context.Background(), os.Getenv("GCP_PROJECT"))
+		if err != nil {
+			log.Println(err)
+		}
+
+		logger = client.Logger("functions-test1-id")
+		slog = logger.StandardLogger(logging.Info)
+		slog2 = logger.StandardLogger(logging.Error)
+	})
+
+	slog.Println("Stackdriver logging print")
+	slog2.Println("Stackdriver logging error?")
+	slog.Panicln("Stackdriver logging panic")
+
 }
--- test1/go.mod
+++ test1/go.mod
@@ -1,1 +1,11 @@
 module test1
+
+require (
+	cloud.google.com/go v0.31.0
+	github.com/googleapis/gax-go v2.0.0+incompatible // indirect
+	go.opencensus.io v0.18.0 // indirect
+	golang.org/x/net v0.0.0-20181029044818-c44066c5c816 // indirect
+	google.golang.org/api v0.0.0-20181030195507-c4edf6535eee // indirect
+	google.golang.org/genproto v0.0.0-20181029155118-b69ba1387ce2 // indirect
+	google.golang.org/grpc v1.16.0 // indirect
+)
