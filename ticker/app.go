b36f1814a41fba57dc9f249829674fa78ebe5428
Sean Liao <seankhliao@gmail.com>
2018-11-02 17:08:14 +0800

    app engine ticker

---
+++ ticker/app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+)
+
+var (
+	client *pubsub.Client
+
+	envPort    = os.Getenv("PORT")
+	envProject = os.Getenv("GCP_PROJECT")
+)
+
+func init() {
+	var err error
+
+	if client, err = pubsub.NewClient(context.Background(), envProject); err != nil {
+		log.Fatal("Init: Create PubSub Client failed: ", err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/fwatch", fwatchHandler)
+
+	port := envPort
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func fwatchHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	_, err := client.Topic("igtools-tick").Publish(ctx, &pubsub.Message{Data: []byte("app engine trigger fwatch")}).Get(ctx)
+	if err != nil {
+		log.Println("Fwatch trigger error: ", err)
+	}
+}
---
+++ ticker/go.mod
@@ -0,0 +1,12 @@
+module github.com/seankhliao/igtools/ticker
+
+require (
+	cloud.google.com/go v0.32.0
+	github.com/googleapis/gax-go v2.0.0+incompatible // indirect
+	go.opencensus.io v0.18.0 // indirect
+	golang.org/x/net v0.0.0-20181102050134-b7e296877c6e // indirect
+	golang.org/x/oauth2 v0.0.0-20181102003913-e0f2c55a7fc7 // indirect
+	google.golang.org/api v0.0.0-20181101214801-45a606955d1c // indirect
+	google.golang.org/genproto v0.0.0-20181101192439-c830210a61df // indirect
+	google.golang.org/grpc v1.16.0 // indirect
+)
