b9f484069a929161718d38dbd8b8c331b72d69b8
Sean Liao <seankhliao@gmail.com>
2018-11-04 12:08:03 +0800

    add more logging

--- test-static/prox.go
+++ test-static/prox.go
@@ -16,12 +16,21 @@ var rp *httputil.ReverseProxy
 
 // P proxies requests to compiled_binary
 func P(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("recovered: ", r)
+		}
+	}()
 	// setup
 	s.Do(func() {
+		log.Println("initializing...")
 		cmd = exec.Command("./compiled_binary")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		cmd.Start()
+		err := cmd.Start()
+		if err != nil {
+			log.Println(err)
+		}
 		url, err := url.Parse("localhost:8080")
 		if err != nil {
 			log.Println(err)
