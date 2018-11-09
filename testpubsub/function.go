befedf2eb08b86497e19200b0a181c0e73393bd8
Sean Liao <seankhliao@gmail.com>
2018-11-09 13:38:00 +0800

    test pubsub 2

--- testpubsub/function.go
+++ testpubsub/function.go
@@ -1,8 +1,14 @@
 package fun
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
+	"cloud.google.com/go/pubsub"
 )
 
 type Msg struct {
@@ -14,5 +20,25 @@ type Msg struct {
 
 func P(c context.Context, msg Msg) error {
 	fmt.Println("Received: ", msg)
+	buf := bytes.Buffer{}
+	m2 := Msg{
+		Data:       []byte("string to data byte"),
+		TestString: "string test publish",
+		TestBool:   false,
+		TestInt:    411,
+	}
+	err := json.NewEncoder(&buf).Encode(m2)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if msg.TestBool {
+		cl, err := pubsub.NewClient(context.Background(), os.Getenv("GCP_PROJECT"))
+		if err != nil {
+			log.Println(err)
+		}
+		cl.Topic("testpubsub").Publish(context.Background(), &pubsub.Message{Data: buf.Bytes()})
+
+	}
 	return nil
 }
