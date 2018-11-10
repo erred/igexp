87562257664fd3fe66b5ba892c368dab463b50d1
Sean Liao <seankhliao@gmail.com>
2018-11-10 14:29:39 +0800

    refactor

---
+++ mkeep/client.go
@@ -0,0 +1,265 @@
+package mkeep
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"sync"
+
+	"github.com/seankhliao/igtools/goinsta"
+
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/storage"
+)
+
+// Blacklist of media feeds to keep
+// {
+//	userID : ["tagged", "feed"]
+// }
+// story, feed, tag
+var (
+	blacklistStory = "story"
+	blacklistFeed  = "feed"
+	blacklistTag   = "tag"
+)
+
+// Blacklist of media sources
+type Blacklist map[int64][]string
+
+// Downlist is a list of media we already have
+type Downlist map[int64]map[string]struct{}
+
+// Client is a singleton of clients
+type Client struct {
+	bucket    *storage.BucketHandle
+	ig        *goinsta.Instagram
+	once      sync.Once
+	topic     *pubsub.Topic
+	downlist  Downlist
+	blacklist Blacklist
+}
+
+func (c *Client) setup() {
+	var err error
+	ctx := context.Background()
+
+	// storage
+	store, err := storage.NewClient(ctx)
+	if err != nil {
+		panic(fmt.Errorf("Login Error: cloud storage failed: %v", err))
+	}
+	c.bucket = store.Bucket(os.Getenv(envBucket))
+
+	// pubsub
+	psc, err := pubsub.NewClient(ctx, os.Getenv(envProject))
+	if err != nil {
+		panic(fmt.Errorf("Login Error: pubsub client failed:%v", err))
+	}
+	c.topic = psc.Topic(os.Getenv(envTopic))
+
+	// goinsta
+	r, err := c.bucket.Object(objGoinsta).NewReader(ctx)
+	if err != nil {
+		panic(fmt.Errorf("Login Error: import %v failed: %v", objGoinsta, err))
+	}
+	defer r.Close()
+
+	c.ig, err = goinsta.ImportReader(r)
+	if err != nil {
+		panic(fmt.Errorf("Import Error: %v", err))
+	}
+
+	// downlist
+	r, err = c.bucket.Object(objDownlist).NewReader(ctx)
+	switch err {
+	case nil:
+		defer r.Close()
+		if err := json.NewDecoder(r).Decode(c.downlist); err != nil {
+			panic(fmt.Errorf("%v decode error: %v", objDownlist, err))
+		}
+	case storage.ErrObjectNotExist:
+	default:
+		panic(fmt.Errorf("%v reader error: %v", objDownlist, err))
+	}
+
+	// blacklist
+	r, err = c.bucket.Object(objBlacklist).NewReader(ctx)
+	switch err {
+	case nil:
+		defer r.Close()
+		if err := json.NewDecoder(r).Decode(&c.blacklist); err != nil {
+			panic(fmt.Errorf("%v decode error: %v", objBlacklist, err))
+		}
+		fmt.Println("Successfully imported blacklist.json")
+	case storage.ErrObjectNotExist:
+	default:
+		panic(fmt.Errorf("%v reader error: %v", objBlacklist, err))
+	}
+
+}
+
+// save saves the current follows back to storage
+func (c *Client) save() error {
+	ctx := context.Background()
+
+	// Goinsta state
+	w := c.bucket.Object(objGoinsta).NewWriter(ctx)
+	defer w.Close()
+	if err := goinsta.Export(c.ig, w); err != nil {
+		return fmt.Errorf("goinsta export failed: %v", err)
+	}
+
+	// downlist
+	w = c.bucket.Object(objGoinsta).NewWriter(ctx)
+	defer w.Close()
+	if err := json.NewEncoder(w).Encode(c.downlist); err != nil {
+		return fmt.Errorf("downlist export failed: %v", err)
+	}
+	return nil
+}
+
+func (c *Client) isBlacklisted(userID int64, feed string) bool {
+	list, ok := c.blacklist[userID]
+	if !ok {
+		return false
+	}
+	for _, it := range list {
+		if it == feed {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Client) isDownloaded(userID int64, mediaID string) bool {
+	u, ok := c.downlist[userID]
+	if !ok {
+		return false
+	}
+	_, ok = u[mediaID]
+	if !ok {
+		return false
+	}
+	return true
+}
+
+func (c *Client) getUsers() error {
+	following := c.ig.Account.Following()
+	for following.Next() {
+		for _, user := range following.Users {
+			if !c.isBlacklisted(user.ID, "") {
+				c.queueUser(user)
+			}
+		}
+	}
+	return nil
+}
+
+func (c *Client) getFeeds(msg Message) error {
+	user := msg.User
+	if !c.isBlacklisted(user.ID, blacklistStory) {
+		feed := user.Stories()
+		for feed.Next() {
+			c.getItems(feed.Items)
+		}
+	}
+	if !c.isBlacklisted(user.ID, blacklistFeed) {
+		feed := user.Feed()
+		for feed.Next() {
+			c.getItems(feed.Items)
+		}
+
+	}
+	if !c.isBlacklisted(user.ID, blacklistTag) {
+		feed, err := user.Tags([]byte{})
+		if err != nil {
+			log.Printf("get tagged for %v, %v error: %v", user.ID, user.Username, err)
+			return nil
+		}
+		for feed.Next() {
+			c.getItems(feed.Items)
+		}
+	}
+	return nil
+}
+
+func (c *Client) getItems(items []goinsta.Item) {
+	for _, item := range items {
+		if len(item.CarouselMedia) != 0 {
+			breakout := false
+			for _, it := range item.CarouselMedia {
+				if c.isDownloaded(item.User.ID, it.ID) {
+					breakout = true
+					break
+				}
+				c.queueItem(it)
+			}
+			if breakout {
+				break
+			}
+			continue
+		}
+
+		if c.isDownloaded(item.User.ID, item.ID) {
+			break
+		}
+		c.queueItem(item)
+	}
+}
+
+func (c *Client) queueUser(user goinsta.User) {
+	buf := bytes.Buffer{}
+	if err := json.NewEncoder(&buf).Encode(newUserMessage(user)); err != nil {
+		log.Println("queueUser encode: ", err)
+	}
+
+	ctx := context.Background()
+	if _, err := c.topic.Publish(ctx, &pubsub.Message{Data: buf.Bytes()}).Get(ctx); err != nil {
+		log.Println("queueUser publish: ", err)
+	}
+
+}
+
+func (c *Client) queueItem(item goinsta.Item) {
+	buf := bytes.Buffer{}
+	if err := json.NewEncoder(&buf).Encode(newItemMessage(item)); err != nil {
+		log.Println("queueItem encode: ", err)
+	}
+
+	ctx := context.Background()
+	if _, err := c.topic.Publish(ctx, &pubsub.Message{Data: buf.Bytes()}).Get(ctx); err != nil {
+		log.Println("queueItem publish: ", err)
+	}
+
+}
+
+func (c *Client) download(msg Message) error {
+	resp, err := c.ig.Client().Get(msg.Url)
+	if err != nil {
+		return fmt.Errorf("Download item %v failed: %v", msg.ItemID, err)
+	}
+	defer resp.Body.Close()
+
+	obj := path.Join(objBase, "media", msg.UserID, msg.ItemID+msg.Ext)
+	w := c.bucket.Object(obj).NewWriter(context.Background())
+	defer w.Close()
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		return fmt.Errorf("Upload item %v failed: %v", msg.ItemID, err)
+	}
+
+	// update downlist
+	id, err := strconv.ParseInt(msg.UserID, 10, 64)
+	if err != nil {
+		return fmt.Errorf("Failed to parse UserID as int64: %v", err)
+	}
+	c.downlist[id][msg.ItemID] = struct{}{}
+
+	return c.save()
+}
--- mkeep/function.go
+++ mkeep/function.go
@@ -1,25 +1,20 @@
 package mkeep
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/url"
-	"os"
 	"path"
 	"strconv"
-	"sync"
-
-	"github.com/seankhliao/igtools/goinsta"
 
 	"cloud.google.com/go/pubsub"
-	"cloud.google.com/go/storage"
+	"github.com/seankhliao/igtools/goinsta"
 )
 
 var (
-	c = client{}
+	client = Client{}
 
 	// names of environment variables
 	envBucket  = "BUCKET"
@@ -33,80 +28,8 @@ var (
 	objGoinsta   = "mkeep/goinsta.json"
 )
 
-type client struct {
-	bucket   *storage.BucketHandle
-	ig       *goinsta.Instagram
-	once     sync.Once
-	topic    *pubsub.Topic
-	downlist Downlist
-}
-
-func (cl *client) Login() {
-	var err error
-	ctx := context.Background()
-
-	// storage
-	store, err := storage.NewClient(ctx)
-	if err != nil {
-		panic(fmt.Errorf("Login Error: cloud storage failed: %v", err))
-	}
-	cl.bucket = store.Bucket(os.Getenv(envBucket))
-
-	// pubsub
-	psc, err := pubsub.NewClient(ctx, os.Getenv(envProject))
-	if err != nil {
-		panic(fmt.Errorf("Login Error: pubsub client failed:%v", err))
-	}
-	cl.topic = psc.Topic(os.Getenv(envTopic))
-
-	// goinsta
-	r, err := cl.bucket.Object(objGoinsta).NewReader(ctx)
-	if err != nil {
-		panic(fmt.Errorf("Login Error: import %v failed: %v", objGoinsta, err))
-	}
-	defer r.Close()
-
-	cl.ig, err = goinsta.ImportReader(r)
-	if err != nil {
-		panic(fmt.Errorf("Import Error: %v", err))
-	}
-
-	// downlist
-	r, err = cl.bucket.Object(objDownlist).NewReader(ctx)
-	if err != nil {
-		if err != storage.ErrObjectNotExist {
-			panic(fmt.Errorf("Login Error: import %v failed: %v", objDownlist, err))
-		}
-	} else {
-		defer r.Close()
-
-		if err = json.NewDecoder(r).Decode(&cl.downlist); err != nil {
-			panic(fmt.Errorf("Decode downlist: %v", err))
-		}
-
-	}
-}
-
-// save saves the current follows back to storage
-func (cl *client) save() {
-	ctx := context.Background()
-
-	// Goinsta state
-	w := cl.bucket.Object(objGoinsta).NewWriter(ctx)
-	defer w.Close()
-	if err := goinsta.Export(c.ig, w); err != nil {
-		log.Println("Goinsta export failed: ", err)
-	}
-
-	// downlist
-	w = cl.bucket.Object(objGoinsta).NewWriter(ctx)
-	defer w.Close()
-	if err := json.NewEncoder(w).Encode(cl.downlist); err != nil {
-		log.Println("Downlist export failed: ", err)
-	}
-}
-
-func Mkeep(ctx context.Context, msg pubsub.Message) error {
+// Mkeep function entry point, decides where to pass the message
+func Mkeep(ctx context.Context, psmsg pubsub.Message) error {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("panic: ", r)
@@ -114,245 +37,83 @@ func Mkeep(ctx context.Context, msg pubsub.Message) error {
 	}()
 
 	// ensure login
-	c.once.Do(c.Login)
+	client.once.Do(client.setup)
 
-	// trigger
-	var di DownloadItem
-	if err := json.Unmarshal(msg.Data, &di); err != nil {
-		return fmt.Errorf("failed to unmarshal pubsub message %v", err)
+	msg, err := parseMessage(psmsg)
+	if err != nil {
+		return fmt.Errorf("Parse pubsub message: %v", err)
 	}
 
-	if di.ExternalTrigger {
-		fmt.Println("External trigger", di)
-		a, err := newArchive()
-		if err != nil {
-			return fmt.Errorf("newArchive failed: %v", err)
-		}
-
-		a.getNewMedia()
-	} else {
-		fmt.Println("Internal trigger", di)
-
-		// itname := path.Join(objBase, "media", di.UserID, di.ItemID+di.Ext)
-		// w := c.bucket.Object(itname).NewWriter(context.Background())
-		// defer w.Close()
-		//
-		// res, err := c.ig.Client().Get(di.Url)
-		// if err != nil {
-		// 	log.Println("failed to download: ", err)
-		// }
-		// defer res.Body.Close()
-		//
-		// io.Copy(w, res.Body)
-
-		// uid, err := strconv.ParseInt(di.UserID, 10, 64)
-		// if err != nil {
-		// 	log.Println("failed to parse int: ", err)
-		// }
-		// c.downlist[uid][di.ItemID] = struct{}{}
-		fmt.Println("finished download: ", di.ItemID)
+	switch msg.Mode {
+	case ModeAll:
+		fmt.Println("Getting all users")
+		return client.getUsers()
+	case ModeUser:
+		fmt.Println("Getting all feeds for ", msg.User.Username)
+		return client.getFeeds(msg)
+	case ModeItem:
+		fmt.Println("Getting item ", msg.ItemID, " for ", msg.UserID)
+		return client.download(msg)
+	default:
+		return fmt.Errorf("Unknown msg mode: %v", msg.Mode)
 	}
-
-	// c.save()
-
-	return nil
 }
 
-// Blacklist of media feeds to keep
-// {
-//	userID : ["tagged", "feed"]
-// }
-// story, feed, tag
 var (
-	blacklistStory = "story"
-	blacklistFeed  = "feed"
-	blacklistTag   = "tag"
+	// Modes for Message
+	ModeAll  = 0
+	ModeUser = 1
+	ModeItem = 2
 )
 
-type Blacklist map[int64][]string
-
-// Downlist is a list of media we already have
-type Downlist map[int64]map[string]struct{}
-
-type archive struct {
-	blacklist Blacklist
-	downlist  Downlist
+// Message comm through pubsub
+type Message struct {
+	Mode   int
+	User   goinsta.User
+	UserID string
+	ItemID string
+	Ext    string
+	Url    string
 }
 
-func newArchive() (*archive, error) {
-	ctx := context.Background()
-	a := archive{}
-
-	r, err := c.bucket.Object(objBlacklist).NewReader(ctx)
-	switch err {
-	case nil:
-		defer r.Close()
-		if err := json.NewDecoder(r).Decode(&a.blacklist); err != nil {
-			return &a, fmt.Errorf("%v decode error: %v", objBlacklist, err)
-		}
-		fmt.Println("Successfully imported blacklist.json")
-	case storage.ErrObjectNotExist:
-	default:
-		return &a, fmt.Errorf("%v reader error: %v", objBlacklist, err)
+func parseMessage(psmsg pubsub.Message) (Message, error) {
+	var msg Message
+	if err := json.Unmarshal(psmsg.Data, &msg); err != nil {
+		return msg, fmt.Errorf("json unmarshal: %v", err)
 	}
-
-	r, err = c.bucket.Object(objDownlist).NewReader(ctx)
-	switch err {
-	case nil:
-		defer r.Close()
-		if err := json.NewDecoder(r).Decode(&a.downlist); err != nil {
-			return &a, fmt.Errorf("%v decode error: %v", objDownlist, err)
-		}
-		fmt.Println("Successfully imported downlist.json")
-	case storage.ErrObjectNotExist:
-	default:
-		return &a, fmt.Errorf("%v reader error: %v", objDownlist, err)
-	}
-
-	return &a, nil
+	return msg, nil
 }
 
-func (a archive) blacklisted(userID int64, feed string) bool {
-	list, ok := a.blacklist[userID]
-	if !ok {
-		return false
-	}
-	for _, it := range list {
-		if it == feed {
-			return true
-		}
-	}
-	return false
-}
-
-func (a archive) downloaded(userID int64, mediaID string) bool {
-	u, ok := a.downlist[userID]
-	if !ok {
-		return false
-	}
-	_, ok = u[mediaID]
-	if !ok {
-		return false
-	}
-	return true
-}
-
-func (a *archive) getItems(its []goinsta.Item, u goinsta.User) {
-	for _, it := range its {
-
-		if len(it.CarouselMedia) != 0 {
-			breakout := false
-			for _, i := range it.CarouselMedia {
-				if a.downloaded(u.ID, i.ID) {
-					breakout = true
-					break
-				}
-				if err := queue(i); err != nil {
-					log.Println("queue failed: ", err)
-				}
-			}
-			if breakout {
-				break
-			}
-			continue
-		}
-
-		if a.downloaded(u.ID, it.ID) {
-			break
-		}
-		if err := queue(it); err != nil {
-			log.Println("queue failed: ", err)
-		}
+func newUserMessage(user goinsta.User) Message {
+	return Message{
+		Mode: ModeUser,
+		User: user,
 	}
 
 }
 
-func (a *archive) getUserMedia(u goinsta.User) {
-	if !a.blacklisted(u.ID, blacklistStory) {
-		stories := u.Stories()
-		for stories.Next() {
-			a.getItems(stories.Items, u)
-		}
-	}
-
-	if !a.blacklisted(u.ID, blacklistFeed) {
-		feed := u.Feed()
-		for feed.Next() {
-			a.getItems(feed.Items, u)
-		}
-	}
-
-	if !a.blacklisted(u.ID, blacklistTag) {
-		feed, err := u.Tags([]byte{})
-		if err != nil {
-			log.Printf("getNewMedia pre get tags for %v, err: %v", u.Username, err)
-		}
-		for feed.Next() {
-			a.getItems(feed.Items, u)
-		}
-
-	}
-
-}
-
-func (a *archive) getNewMedia() {
-	following := c.ig.Account.Following()
-	counter := 0
-	for following.Next() {
-		for _, u := range following.Users {
-			counter++
-			fmt.Println("Processing user: #", counter, " ", u.Username)
-			a.getUserMedia(u)
-
-			if counter > 1 {
-				return
-			}
-		}
-	}
-}
-
-func queue(item goinsta.Item) error {
-	buf := bytes.Buffer{}
-	if err := json.NewEncoder(&buf).Encode(NewDownloadItem(item)); err != nil {
-		return fmt.Errorf("encode failed: %v", err)
-	}
-	msg := pubsub.Message{Data: buf.Bytes()}
-
-	ctx := context.Background()
-	if _, err := c.topic.Publish(ctx, &msg).Get(ctx); err != nil {
-		return fmt.Errorf("queue failed: %v", err)
-	}
-	return nil
-}
-
-type DownloadItem struct {
-	ExternalTrigger bool
-	UserID          string
-	ItemID          string
-	Ext             string
-	Url             string
-}
-
-func NewDownloadItem(it goinsta.Item) DownloadItem {
+func newItemMessage(item goinsta.Item) Message {
 	var link string
-	if len(it.Images.Versions) > 0 {
-		link = it.Images.GetBest()
-		link = goinsta.GetBest(it.Images.Versions)
+	if len(item.Images.Versions) > 0 {
+		link = item.Images.GetBest()
+		link = goinsta.GetBest(item.Images.Versions)
 	} else {
-		link = goinsta.GetBest(it.Videos)
+		link = goinsta.GetBest(item.Videos)
 	}
 	if link == "" {
-		log.Println("empty link: ", it.Videos, it.Images)
+		log.Println("empty link: ", item.Videos, item.Images)
 	}
 	u, err := url.Parse(link)
 	if err != nil {
 		log.Println("failed to parse url: ", err)
-		return DownloadItem{}
+		return Message{}
 	}
-	return DownloadItem{
-		UserID: strconv.FormatInt(it.User.ID, 10),
-		ItemID: it.ID,
+	return Message{
+		Mode:   ModeItem,
+		UserID: strconv.FormatInt(item.User.ID, 10),
+		ItemID: item.ID,
 		Ext:    path.Ext(u.Path),
 		Url:    link,
 	}
+
 }
